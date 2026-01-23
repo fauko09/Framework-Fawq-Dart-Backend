bc001dc29b339327b8679dc72f9de52981eb7237
fauko missalam <kauko71@gmail.com>
2026-01-23 11:24:46 +0700

    ecosystem pm2 js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: "fawq_dart_backend",
+    script: "./build/main",
+    exec_mode: "fork",
+    instances: 1,
+
+    out_file: "./logs/out.log",
+    error_file: "./logs/error.log",
+    merge_logs: true,
+
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 3000
+  }]
+}
